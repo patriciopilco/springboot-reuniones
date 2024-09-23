dd0ca7df1061a369c7f77451eba5070796598f06
Patricio Pilco <ppilco@galavail.com>
2024-09-23 15:36:32 -0500

    first commit

---
+++ src/test/java/com/xpoch/reuniones/SpringbootReunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpoch.reuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
